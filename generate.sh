4c366e766279d5e6168567e5d7cf9f9342f32f4d
San7o <santigio2003@gmail.com>
2026-02-01 09:02:35 +0100

    openssl: added certificate generation via openssl cli
    Signed-off-by: San7o <santigio2003@gmail.com>

---
+++ generate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#
+# Generate a self-signed certificate with openssl
+#
+
+PUBLIC_KEY_NAME=public.pem
+PRIVATE_KEY_NAME=private.key
+
+C=US
+STATE_NAME=Oregon
+CITY_NAME=Portland
+COMPANY_NAME=Company Name
+COMPANY_SECTION_NAME=Org
+COMPANY_HOSTNAME=www.example.com
+
+openssl req -x509 \
+    -newkey rsa:4096 \
+    -keyout $PUBLIC_KEY_NAME \
+    -out $PRIVATE_KEY_NAME \
+    -sha256 \
+    -days 3650 \
+    -nodes \
+    -subj "/C=$C/ST=$STATE_NAME/L=$CITY_NAME/O=$COMPANY_NAME/OU=$COMPANY_SECTION_NAME/CN=$COMPANY_HOSTNAME"
